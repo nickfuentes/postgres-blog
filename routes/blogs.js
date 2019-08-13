d686ec4c116d2d4f0eb0e490d429f1afe6485662
Full Name <nickfuentes24@gmail.com>
2019-08-13 11:49:40 -0500

    Changed some comments on the routes to describe it better.

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,7 +5,7 @@ const pgp = require('pg-promise')();
 const connectionString = 'postgres://localhost:5432/blogdb';
 const db = pgp(connectionString);
 
-// GET shows all the blogs
+// GET gets all the blogs
 router.get('/', (req, res) => {
     db.any('SELECT blogid, title, author, body, datecreated FROM blogs;')
         .then(blogs => {
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
         })
 })
 
-// GET to show the blog create form
+// GET gets the blog create form
 router.get("/create-blog", (req, res) => {
     res.render("create-blog")
 })
@@ -35,10 +35,35 @@ router.post('/create-blog', (req, res) => {
 // POST deletes the blog
 router.post("/delete-blog", (req, res) => {
     let blogid = req.body.blogid
-    console.log(blogid)
-    db.none('DELETE FROM blogs WHERE blogid = $1', [blogid]).then(() => {
-        res.redirect('/blogs')
-    })
+
+    db.none('DELETE FROM blogs WHERE blogid = $1', [blogid])
+        .then(() => {
+            res.redirect('/blogs')
+        })
 })
+
+// // GET gets the update blog form
+// router.get("/update-blog", (req, res) => {
+//     let blogid = req.body.blogid
+//     console.log(blogid)
+//     db.any('SELECT blogid, title, author, body, datecreated FROM blogs WHERE blogid = $1', [blogid])
+//         .then(blogs => {
+//             console.log(blogs)
+//             res.render('update-blog', { blogs: blogs })
+//         })
+
+//     res.render('update-blog')
+// })
+
+// router.post("/update-blog", (req, res) => {
+//     let blogid = req.body.blogid
+//     let title = req.body.title
+//     let author = req.body.author
+//     let body = req.body.body
+
+//     db.none('UPDATE blogs SET title = $1, author = $2, body = $3, WHERE blogid = $1', [title, author, body, blogid]).then(() => {
+//         res.redirect('/blogs')
+//     })
+// })
 
 module.exports = router

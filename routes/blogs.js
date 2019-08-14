29f47c9b26a676b4bdb9b079b7a667058401bedc
Full Name <nickfuentes24@gmail.com>
2019-08-13 22:56:59 -0500

    Added bcrypt to password before reaching the databse.

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const SALT_ROUNDS = 10
 
 
 const pgp = require('pg-promise')();
@@ -8,68 +10,70 @@ const db = pgp(connectionString);
 
 // GET gets all the blogs
 router.get('/', (req, res) => {
-    db.any('SELECT blogid, title, body, datecreated FROM blogs;')
-        .then(blogs => {
-            console.log(blogs)
-            res.render('blogs', { blogs: blogs })
-        }).catch(error => {
-            console.log(error)
-            res.render('blogs', { message: 'Unable to get blogs!' })
-        })
+  db.any('SELECT blogid, title, body, datecreated FROM blogs;')
+    .then(blogs => {
+      console.log(blogs)
+      res.render('blogs', { blogs: blogs })
+    }).catch(error => {
+      console.log(error)
+      res.render('blogs', { message: 'Unable to get blogs!' })
+    })
 })
 
 // POST the register information to database
 router.get('/register', (req, res) => {
-    res.render('register')
+  res.render('register')
 })
 
 // POST the user username and password to users database
 router.post('/register', (req, res) => {
 
-    let username = req.body.username
-    let password = req.body.password
+  let username = req.body.username
+  let password = req.body.password
 
-    db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username])
-        .then((user) => {
-            if (user) {
-                res.render('register', { message: "User name already exists!" })
-            } else {
-                db.none('INSERT INTO users(username, password) VALUES($1, $2)', res[username, password])
-                    .then(() => {
-                        res.send('SUCCESS')
-                    })
-            }
+  db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username])
+    .then((user) => {
+      if (user) {
+        res.render('register', { message: "User name already exists!" })
+      } else {
+        bcrypt.hash(password, SALT_ROUNDS).then(function (hash) {
+          db.none('INSERT INTO users(username, password) VALUES($1, $2)', [username, hash])
+            .then(() => {
+              res.send('SUCCESS')
+            })
         })
+      }
+    })
 })
 
 // GET shows the login form
 router.get('/login', (req, res) => {
-    res.render('login')
+  res.render('login')
 })
 
 // GET gets the blog create form
 router.get("/create-blog", (req, res) => {
-    res.render("create-blog")
+  res.render("create-blog")
 })
 
 // POST the blog to the database
 router.post('/create-blog', (req, res) => {
-    let title = req.body.title
-    let body = req.body.body
+  let title = req.body.title
+  let body = req.body.body
 
-    db.none('INSERT INTO blogs(title, body) VALUES($1, $2)', [title, body]).then(() => {
-        res.redirect('/blogs')
-    })
+  db.none('INSERT INTO blogs(title, body) VALUES($1, $2)', [title, body]).then(() => {
+    res.redirect('/blogs')
+  })
 })
 
 // POST deletes the blog
 router.post("/delete-blog", (req, res) => {
-    let blogid = req.body.blogid
+  let blogid = req.body.blogid
 
-    db.none('DELETE FROM blogs WHERE blogid = $1', [blogid])
-        .then(() => {
-            res.redirect('/blogs')
-        })
+  db.none('DELETE FROM blogs WHERE blogid = $1', [blogid])
+    .then(() => {
+      res.redirect('/blogs')
+    })
 })
 
 // // GET gets the update blog form

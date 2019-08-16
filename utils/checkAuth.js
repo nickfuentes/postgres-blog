951ca681238007ccc451edb4bd3b2aab18af0084
Full Name <nickfuentes24@gmail.com>
2019-08-15 19:43:07 -0500

    Added local functionality if you are logged in the links log in and register are gone.

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -3,6 +3,7 @@ function checkAuth(req, res, next) {
     if (req.session) {
 
         if (req.session.user) {
+            res.locals.authenticated = true
             next()
         } else {
             res.redirect("/blogs/login")
